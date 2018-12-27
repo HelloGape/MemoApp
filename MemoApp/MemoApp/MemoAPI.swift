46d0a8917df37aa5f4c75bc224afaa82708cb560
Kang Jae Kwon <rkdwornjs123@gmail.com>
2018-12-27 12:52:04 +0900

    created TableView, and add the little code

---
+++ MemoApp/MemoApp/MemoAPI.swift
@@ -0,0 +1,14 @@
+//
+//  MemoAPI.swift
+//  MemoApp
+//
+//  Created by Kang JK on 27/12/2018.
+//  Copyright © 2018 Kang JK. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemoAPI : UIViewController {
+    
+}
