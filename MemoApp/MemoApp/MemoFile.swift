8b4c5a0bd147b405866030b04c5db7c99f1a9914
Kang Jae Kwon <rkdwornjs123@gmail.com>
2019-01-18 20:14:31 +0900

    add some function,(delete,change... etc) and check little code

--- MemoApp/MemoApp/MemoFile.swift
+++ MemoApp/MemoApp/MemoFile.swift
@@ -1,23 +1,35 @@
-//
-//  MemoFile.swift
-//  MemoApp
-//
-//  Created by Kang JK on 27/12/2018.
-//  Copyright © 2018 Kang JK. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
 class MemoFile : UIViewController {
     @IBOutlet weak var memo: UITextView!
+    let memoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
+    var memoData = UserDefaults.standard.object(forKey: "MemoData") as? [String] ?? [String]()
     
-    var getData : String = ""
     override func viewDidLoad() {
-        memo.text = getData
+        if memoNumber == -1 {
+            memo.text = ""
+        } else {
+            memo.text = memoData[memoNumber]
+        }
     }
     @IBAction func saveMemo(_ sender: Any) {
-        MemoAPI.shard.addTheData(new: memo.text)
-        UserDefaults.standard.set(MemoAPI.shard.getMainList(), forKey: "memoData")
+        let MemoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
+        if MemoNumber == -1 {
+            MemoAPI.shard.addTheData(new: memo.text)
+        }else {
+            memoData.remove(at: MemoNumber)
+            memoData.insert(memo.text, at: MemoNumber)
+            UserDefaults.standard.set(memoData, forKey: "MemoData")
+        }
+    }
+    @IBAction func deleteButton(_ sender: Any) {
+        let MemoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
+        if MemoNumber == -1 {
+            
+        }else {
+            memoData.remove(at: MemoNumber)
+            UserDefaults.standard.set(memoData, forKey: "MemoData")
+        }
     }
 }
--- MemoApp/MemoApp/ViewController.swift
+++ MemoApp/MemoApp/ViewController.swift
@@ -1,19 +1,13 @@
-//
-//  ViewController.swift
-//  MemoApp
-//
-//  Created by Kang JK on 26/12/2018.
-//  Copyright © 2018 Kang JK. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var MemoCList: UITableView!
     
+    var MemoData = MemoAPI.shard.getMainList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         MemoCList.dataSource = self
         MemoCList.delegate = self
     }
@@ -21,36 +15,38 @@ class ViewController: UIViewController {
         DispatchQueue.main.async {
             self.MemoCList.reloadData()
         }
-    }
-    @IBAction func createNewMemo(_ sender: Any) {
-        
-    }
+    }   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "savedMemo" {
-            if let sendData = segue.destination as? MemoFile{
-                sendData.getData = sender as? String ?? ""
-            }
+            
+        } else if segue.identifier == "addButton" {
+            
         }
     }
+    @IBAction func addData(_ sender: Any) {
+        UserDefaults.standard.set(-1, forKey: "MemoNumber")
+    }
+    
 }
-
 extension ViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return MemoAPI.shard.getMainList().count
+        MemoData = UserDefaults.standard.object(forKey: "MemoData") as? [String] ?? [""]
+        return MemoData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MemoCell", for: indexPath)
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MemoCell", for: indexPath)
         
         if let addCellName : UILabel = cell.viewWithTag(1) as? UILabel {
-            addCellName.text = MemoAPI.shard.getMainList()[indexPath.row]
+            addCellName.text = MemoData[indexPath.row]
         }
         return cell
     }
 }
 extension ViewController : UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let memoData  = MemoAPI.shard.getMainList()[indexPath.row]
-        performSegue(withIdentifier: "savedMemo", sender: memoData )
+        let memoNumber = indexPath.row
+        UserDefaults.standard.set(memoNumber, forKey: "MemoNumber")
+        performSegue(withIdentifier: "savedMemo", sender: self)
     }
 }
